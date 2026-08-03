71ac91238d99aa1802803b0355fe70dcb7d60ada
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-04 00:22:08 +0530

    Added Inputs

---
+++ Inputs.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int Rollno = input.nextInt();
+
+        System.out.println("Your Roll No is " + Rollno);
+
+    }
+}
--- Primitives.java
+++ Primitives.java
@@ -1,8 +1,11 @@
 public class Primitives {
     public static void main(String[] args){
 
-        int RollNo = 417;
         String Name = "Atul gangwar";  // String is not a primitive data type in Java, but it is widely used for text representation.
+
+        // These all are Primitive Data types in Java
+        
+        int RollNo = 417;
 
         char Grade = 'A';
         float Percentage = 80.95f;
