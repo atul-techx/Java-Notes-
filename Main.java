c78d84b3f87d4117fd0b16279328f17ff3139c28
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-03 23:30:25 +0530

    A little Change in Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 public class Main {     // This class is accessible from anywhere in the project
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println("Hello Atul!");
     }
     
     
