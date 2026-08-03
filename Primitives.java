7ee213da2bad7053d38fe9a7753a311001ff0d08
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-03 23:57:04 +0530

    Added Primitives in the learning path

---
+++ Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives {
+    public static void main(String[] args){
+
+        int RollNo = 417;
+        String Name = "Atul gangwar";  // String is not a primitive data type in Java, but it is widely used for text representation.
+
+        char Grade = 'A';
+        float Percentage = 80.95f;
+
+        double largeDecimalNumber = 83546454956.5587469;
+        long largeIntegerValue = 1234567890L;
+
+        boolean isIPassed = true;
+        boolean isINotEligibleForNextClass = false;
+    }
+}
